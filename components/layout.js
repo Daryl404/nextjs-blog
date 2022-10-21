04440421ac37abb7aba3fd5ce4b3ea7003fd2b6c
Daryl Estrada <frdarylestrada@gmail.com>
2022-10-21 09:34:34 +0700

    Import images

--- components/layout.js
+++ components/layout.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
+import Profile from '../public/images/profile.gif'
 
 const name = 'Daryl';
 export const siteTitle = 'Next.js Sample Website';
@@ -30,7 +31,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile1.JPG"
+              src={Profile}
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -44,7 +45,7 @@ export default function Layout({ children, home }) {
               <a>
                 <Image
                   priority
-                  src="/images/profile1.JPG"
+                  src={Profile}
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
